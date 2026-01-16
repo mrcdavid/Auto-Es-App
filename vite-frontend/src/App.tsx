dafa88628080c566d9b0011f02574f2ff8211c38
Marc <marcallen.david.18@gmail.com>
2026-01-16 15:57:06 +0800

    Added Register

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import ProtectedPage from "./Pages/Protected";
+import RegisterPage from "./Pages/Register";
 import "./index.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/protected" element={<ProtectedPage />} />
       </Routes>
     </Router>
